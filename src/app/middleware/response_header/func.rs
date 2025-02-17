31c116f3f4544323620e4b38df11f6504ddc956c
尤雨东 <root@ltpp.vip>
2025-02-17 09:07:31 +0800

    feat: keep-alive

--- src/app/middleware/response_header/func.rs
+++ src/app/middleware/response_header/func.rs
@@ -4,5 +4,6 @@ pub fn response_header(arc_lock_controller_data: ArcRwLockControllerData) {
     let mut controller_data: RwLockWriteControllerData =
         get_rw_lock_write_controller_data(&arc_lock_controller_data);
     let response: &mut Response = controller_data.get_mut_response();
-    response.set_header("server", config::server::constant::SERVER_NAME);
+    response.set_header(SERVER, config::server::constant::SERVER_NAME);
+    response.set_header(CONNECTION, CONNECTION_KEEP_ALIVE);
 }
