af01a6ff1bdd6a9ef176c50652855f2f4290062c
尤雨东 <root@ltpp.vip>
2025-02-28 08:54:27 +0800

    feat: v0.18.2

--- src/app/controller/favicon_ico/func.rs
+++ src/app/controller/favicon_ico/func.rs
@@ -2,16 +2,17 @@ use crate::*;
 
 pub async fn favicon_ico(arc_lock_controller_data: ArcRwLockControllerData) {
     let data: Vec<u8> = plugin::logo_img::func::get_logo_img();
-    {
-        let mut controller_data: RwLockWriteControllerData =
-            arc_lock_controller_data.get_write_lock().await;
-        let response: &mut Response = controller_data.get_mut_response();
-        response.set_header(CONTENT_TYPE, IMAGE_PNG);
-        response.set_header(CACHE_CONTROL, "public, max-age=3600");
-    }
-    let send_res: ResponseResult = arc_lock_controller_data.send_response(200, data).await;
-    arc_lock_controller_data
-        .get_log()
+    let send_res: ResponseResult = arc_lock_controller_data
+        .set_response_header(CONTENT_TYPE, IMAGE_PNG)
         .await
-        .info(format!("Response result => {:?}", send_res), log_handler);
+        .set_response_header(CACHE_CONTROL, "public, max-age=3600")
+        .await
+        .send_response(200, data)
+        .await;
+    let request: Request = arc_lock_controller_data.get_request().await;
+    arc_lock_controller_data
+        .log_info(format!("Request result => {}", request), log_handler)
+        .await
+        .log_info(format!("Response result => {:?}", send_res), log_handler)
+        .await;
 }
