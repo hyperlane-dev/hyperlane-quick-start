9251d860f8a845752a2edd4d324afb62620bf8bb
尤雨东 <root@ltpp.vip>
2025-03-03 22:04:09 +0800

    feat: v0.21.0

--- src/app/middleware/client/func.rs
+++ src/app/middleware/client/func.rs
@@ -1,11 +1,9 @@
 use crate::*;
 
 pub async fn client(arc_lock_controller_data: ArcRwLockControllerData) {
-    let socket_addr: String = arc_lock_controller_data
-        .get_socket_addr()
-        .await
-        .unwrap_or_default();
+    let socket_addr: OptionSocketAddr = arc_lock_controller_data.get_socket_addr().await;
+    let socket_addr_string: String = socket_addr.unwrap_or(DEFAULT_SOCKET_ADDR).to_string();
     arc_lock_controller_data
-        .set_response_header("SocketAddr", socket_addr)
+        .set_response_header("SocketAddr", socket_addr_string)
         .await;
 }
