4eea19537e088105d835a4e8b5ce142e015a0ddb
尤雨东 <root@ltpp.vip>
2025-03-14 22:59:06 +0800

    feat: 0.31.0

--- src/app/middleware/request/client/func.rs
+++
@@ -1,9 +0,0 @@
-use crate::*;
-
-pub async fn client(controller_data: ControllerData) {
-    let socket_addr: OptionSocketAddr = controller_data.get_socket_addr().await;
-    let socket_addr_string: String = socket_addr.unwrap_or(DEFAULT_SOCKET_ADDR).to_string();
-    controller_data
-        .set_response_header("SocketAddr", socket_addr_string)
-        .await;
-}
