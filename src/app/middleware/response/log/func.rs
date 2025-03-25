103981c9f0efcd0cbddf6d682fb40fbaf5820be3
尤雨东 <root@ltpp.vip>
2025-03-25 12:26:45 +0800

    feat: v0.45.2

--- src/app/middleware/response/log/func.rs
+++ src/app/middleware/response/log/func.rs
@@ -4,8 +4,8 @@ pub async fn log(controller_data: ControllerData) {
     let request: String = controller_data.get_request().await.get_string();
     let response: String = controller_data.get_response().await.get_string();
     controller_data
-        .log_info(request, log_handler)
+        .log_info(&request, log_handler)
         .await
-        .log_info(response, log_handler)
+        .log_info(&response, log_handler)
         .await;
 }
