35d1ccd516743e4a0bb00191a368185f4d8dfc25
尤雨东 <root@ltpp.vip>
2025-04-03 09:25:35 +0800

    feat: v0.50.0

--- src/app/middleware/response/log/func.rs
+++ src/app/middleware/response/log/func.rs
@@ -3,8 +3,8 @@ use crate::*;
 pub async fn log(ctx: Context) {
     let request: String = ctx.get_request().await.get_string();
     let response: String = ctx.get_response().await.get_string();
-    ctx.log_info(&request, log_handler)
+    ctx.async_log_info(&request, log_handler)
         .await
-        .log_info(&response, log_handler)
+        .async_log_info(&response, log_handler)
         .await;
 }
