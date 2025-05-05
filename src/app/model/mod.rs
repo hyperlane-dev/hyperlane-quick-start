0bbd2c71fdd1d65b7c3541246ffc375828f47b33
尤雨东 <root@ltpp.vip>
2025-05-05 09:13:06 +0800

    feat: v0.77.0

--- src/app/model/mod.rs
+++ src/app/model/mod.rs
@@ -1,9 +1,10 @@
+pub(crate) mod application;
 pub(crate) mod bean;
-pub(crate) mod bo;
+pub(crate) mod business;
+pub(crate) mod data;
+pub(crate) mod data_access;
+pub(crate) mod data_transfer;
 pub(crate) mod domain;
-pub(crate) mod dto;
-pub(crate) mod entity;
 pub(crate) mod param;
-pub(crate) mod po;
-pub(crate) mod pojo;
-pub(crate) mod vo;
+pub(crate) mod persistent;
+pub(crate) mod view;
