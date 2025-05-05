feb8bdf3ebdced6f9a8fa0be6c769a100f7071dc
尤雨东 <root@ltpp.vip>
2025-05-05 09:35:07 +0800

    feat: dir

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -2,7 +2,6 @@ pub(crate) mod aspect;
 pub(crate) mod controller;
 pub(crate) mod exception;
 pub(crate) mod filter;
-pub(crate) mod interrupt;
 pub(crate) mod mapper;
 pub(crate) mod middleware;
 pub(crate) mod model;
