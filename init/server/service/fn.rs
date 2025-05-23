7d168908cac7141f97b333ef01b07d94795b7aba
尤雨东 <root@ltpp.vip>
2025-05-23 12:44:20 +0800

    Merge branch 'master' into group-chat

--- init/server/service/fn.rs
+++ init/server/service/fn.rs
@@ -69,9 +69,9 @@ async fn ttl(server: &Server) {
     println_success!("Server ttl: ", SERVER_TTI);
 }
 
-async fn enable_inner_websocket_handle(server: &Server) {
-    server.enable_inner_websocket_handle("/websocket").await;
-    println_success!("Server inner websocket handle enable completed");
+async fn disable_inner_websocket_handle(server: &Server) {
+    server.disable_inner_websocket_handle("/websocket").await;
+    println_success!("Server inner websocket handle disable completed");
 }
 
 async fn register_request_middleware(server: &Server) {
@@ -138,7 +138,7 @@ async fn create_server() {
     nodelay(&server).await;
     http_line_buffer_size(&server).await;
     websocket_buffer_size(&server).await;
-    enable_inner_websocket_handle(&server).await;
+    disable_inner_websocket_handle(&server).await;
     register_request_middleware(&server).await;
     register_route(&server).await;
     register_response_middleware(&server).await;
