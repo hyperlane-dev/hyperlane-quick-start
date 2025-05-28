17513cd6b14afcb56e4dcd3b6e69b6ccea3d83cc
尤雨东 <root@ltpp.vip>
2025-05-29 01:02:14 +0800

    feat: update

--- app/middleware/response/log/fn.rs
+++ app/middleware/response/log/fn.rs
@@ -1,7 +1,7 @@
 use super::*;
 
 pub async fn log(ctx: Context) {
-    if ctx.is_enable_websocket().await {
+    if ctx.get_request().await.upgrade_type_is_websocket() {
         return;
     }
     let request_body_len: usize = ctx.get_request().await.get_body().len();
