a2101d1d9deb600036957b17c7f135da73d5ad66
尤雨东 <root@ltpp.vip>
2025-05-29 09:16:11 +0800

    feat: v1.20.0

--- app/middleware/response/log/fn.rs
+++ app/middleware/response/log/fn.rs
@@ -1,9 +1,6 @@
 use super::*;
 
 pub async fn log(ctx: Context) {
-    let request_body_len: usize = ctx.get_request().await.get_body().len();
-    ctx.set_request_body(format!("binary data len: {request_body_len}"))
-        .await;
     let request: String = ctx.get_request().await.get_string();
     let response: String = ctx.get_response().await.get_string();
     log_info(request).await;
