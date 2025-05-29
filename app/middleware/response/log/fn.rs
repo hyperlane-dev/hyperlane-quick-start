26202a9c739a21bf9f03aab38007cf5c3f1e479c
尤雨东 <root@ltpp.vip>
2025-05-29 09:17:48 +0800

    feat: update

--- app/middleware/response/log/fn.rs
+++ app/middleware/response/log/fn.rs
@@ -4,11 +4,6 @@ pub async fn log(ctx: Context) {
     if ctx.get_request().await.upgrade_type_is_websocket() {
         return;
     }
-    let request_body_len: usize = ctx.get_request().await.get_body().len();
-    ctx.set_request_body(format!("binary data len: {request_body_len}"))
-        .await;
     let request: String = ctx.get_request().await.get_string();
-    let response: String = ctx.get_response().await.get_string();
     log_info(request).await;
-    log_info(response).await
 }
