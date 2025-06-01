3b7b997794c70f73ae90634ebd81c88d7d7ec45a
尤雨东 <root@ltpp.vip>
2025-06-01 23:35:51 +0800

    feat: v

--- init/infrastructure/hyperlane/fn.rs
+++ init/infrastructure/hyperlane/fn.rs
@@ -80,7 +80,7 @@ async fn register_response_middleware(server: &Server) {
 async fn register_route(server: &Server) {
     server.route("/", controller::root::handle).await;
     server
-        .route(format!("/hello/:{NAME_KEY}"), controller::hello::handle)
+        .route(format!("/hello/{NAME_KEY}"), controller::hello::handle)
         .await;
     server
         .route("/websocket", controller::websocket::handle)
