7c2bf782784cb69a6b8ea92c014a1df4eb3b0e03
尤雨东 <root@ltpp.vip>
2025-06-06 23:28:00 +0800

    update: code

--- app/controller/websocket/fn.rs
+++ app/controller/websocket/fn.rs
@@ -1,26 +1,31 @@
 use super::*;
 
 async fn callback(ctx: Context) {
-    let websocket: &WebSocket = get_global_websocket();
-    let receiver_count: ReceiverCount = websocket
-        .receiver_count(BroadcastType::PointToGroup("/"))
-        .unwrap_or_default();
     let body: Vec<u8> = ctx.get_request_body().await;
     ctx.set_response_body(body).await;
-    println_success!(receiver_count);
 }
 
 async fn send_callback(_: Context) {}
+
+async fn client_closed_callback(ctx: Context) {
+    let websocket: &WebSocket = get_global_websocket();
+    let receiver_count: ReceiverCount = websocket
+        .pre_decrement_receiver_count(BroadcastType::PointToGroup("/"))
+        .unwrap_or_default();
+    ctx.set_response_body(format!("Current online client count: {}", receiver_count))
+        .await;
+}
 
 pub async fn handle(ctx: Context) {
     let websocket: &WebSocket = get_global_websocket();
     websocket
         .run(
             &ctx,
-            1024,
+            1_024_000,
             BroadcastType::PointToGroup("/"),
             callback,
             send_callback,
+            client_closed_callback,
         )
         .await;
 }
