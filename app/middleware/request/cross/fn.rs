b1de3378cf912ca6ce457e21d9310a4b73036103
尤雨东 <root@ltpp.vip>
2025-06-09 17:34:26 +0800

    update: code

--- app/middleware/request/cross/fn.rs
+++ app/middleware/request/cross/fn.rs
@@ -1,10 +1,10 @@
 use super::*;
 
 pub async fn cross(ctx: Context) {
-    ctx.set_response_header(ACCESS_CONTROL_ALLOW_ORIGIN, ANY)
+    ctx.set_response_header(ACCESS_CONTROL_ALLOW_ORIGIN, WILDCARD_ANY)
         .await
         .set_response_header(ACCESS_CONTROL_ALLOW_METHODS, ALL_METHODS)
         .await
-        .set_response_header(ACCESS_CONTROL_ALLOW_HEADERS, ANY)
+        .set_response_header(ACCESS_CONTROL_ALLOW_HEADERS, WILDCARD_ANY)
         .await;
 }
