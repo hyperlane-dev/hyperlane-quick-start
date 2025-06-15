ed1b16546b3d607d7310f0d04bc8bd4cddb54adf
尤雨东 <root@ltpp.vip>
2025-06-15 17:09:12 +0800

    update: code

--- config/business/upload/const.rs
+++ config/business/upload/const.rs
@@ -1,4 +1,4 @@
-pub const UPLOAD_DIR: &str = "/shell/upload/";
+pub const UPLOAD_DIR: &str = "/shell/upload";
 pub const CHUNKIFY_DIRECTORY_HEADER: &str = "x-directory-path";
 pub const STATIC_ROUTE: &str = "static";
 pub const UPLOAD_DIR_KEY: &str = "upload_dir";
