cc4906e246bb49eea49cd29a4a309643df231cef
尤雨东 <root@ltpp.vip>
2025-06-26 21:59:04 +0800

    update: code

--- app/model/business/upload/mod.rs
+++ app/model/business/upload/mod.rs
@@ -1,8 +1,13 @@
+mod r#static;
 mod r#struct;
+mod r#type;
 
+pub use r#static::*;
 pub use r#struct::*;
+pub use r#type::*;
 
 use super::*;
 
+use once_cell::sync::Lazy;
 use serde::Serialize;
 use utoipa::ToSchema;
--- app/service/upload/mod.rs
+++ app/service/upload/mod.rs
@@ -1,14 +1,8 @@
 mod r#fn;
 mod r#impl;
-mod r#static;
-mod r#type;
 
 pub use super::*;
 pub use r#fn::*;
-pub use r#static::*;
-pub use r#type::*;
 
 use hyperlane_config::business::upload::*;
 use model::business::upload::*;
-
-use once_cell::sync::Lazy;
