c6860c16cc1fce25c3c41dccfa01fdd1cc30852c
尤雨东 <root@ltpp.vip>
2025-07-06 12:25:15 +0800

    update: code

--- app/middleware/response/log/fn.rs
+++ app/middleware/response/log/fn.rs
@@ -3,5 +3,10 @@ use super::*;
 pub async fn log(ctx: Context) {
     let request: String = ctx.get_request().await.get_string();
     let response: String = ctx.get_response().await.get_string();
+    if *ctx.get_response().await.get_status_code() == 200 {
+        println_success!(request, BR, response);
+    } else {
+        println_warning!(request, BR, response);
+    }
     log_info(format!("{request}{BR}{response}")).await;
 }
