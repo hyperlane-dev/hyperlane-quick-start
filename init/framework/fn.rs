d3858d82b535a7cae8a33fcbba1214594ffbe62a
尤雨东 <root@ltpp.vip>
2025-07-20 13:38:53 +0800

    feat: v1.62.0

--- init/framework/fn.rs
+++ init/framework/fn.rs
@@ -1,37 +1,49 @@
 use super::*;
 
-fn configure_server_basic(server: &Server) {
-    server
-        .host(SERVER_HOST)
-        .port(SERVER_PORT)
-        .set_ttl(SERVER_TTI)
-        .set_linger(SERVER_LINGER)
-        .set_nodelay(SERVER_NODELAY)
-        .error_hook(exception::framework::error_hook)
-        .http_buffer(SERVER_HTTP_BUFFER)
-        .ws_buffer(SERVER_WS_BUFFER);
+async fn configure_server_basic(server: &Server) {
+    server.host(SERVER_HOST).await;
+    server.port(SERVER_PORT).await;
+    server.set_ttl(SERVER_TTI).await;
+    server.set_linger(SERVER_LINGER).await;
+    server.set_nodelay(SERVER_NODELAY).await;
+    server.error_hook(exception::framework::error_hook).await;
+    server.http_buffer(SERVER_HTTP_BUFFER).await;
+    server.ws_buffer(SERVER_WS_BUFFER).await;
 }
 
-fn configure_request_middleware(server: &Server) {
+async fn configure_request_middleware(server: &Server) {
     server
         .request_middleware(middleware::request::cross::cross)
+        .await;
+    server
         .request_middleware(middleware::request::response::response_header)
+        .await;
+    server
         .request_middleware(middleware::request::response::response_status_code)
-        .request_middleware(middleware::request::response::response_body);
+        .await;
+    server
+        .request_middleware(middleware::request::response::response_body)
+        .await;
 }
 
-fn configure_response_middleware(server: &Server) {
+async fn configure_response_middleware(server: &Server) {
     server
         .response_middleware(middleware::response::send::send)
-        .response_middleware(middleware::response::log::log);
+        .await;
+    server
+        .response_middleware(middleware::response::log::log)
+        .await;
 }
 
-fn configure_routes(server: &Server) {
+async fn configure_routes(server: &Server) {
+    server.route("/", controller::root::handle).await;
     server
-        .route("/", controller::root::handle)
         .route(format!("/hello/{{{NAME_KEY}}}"), controller::hello::handle)
-        .route("/websocket", controller::ws::handle)
-        .route("/favicon.ico", controller::favicon_ico::handle);
+        .await;
+    server.route("/websocket", controller::ws::handle).await;
+    server
+        .route("/favicon.ico", controller::favicon_ico::handle)
+        .await;
 }
 
 fn runtime() -> Runtime {
@@ -47,12 +59,13 @@ fn runtime() -> Runtime {
 
 #[hyperlane(server)]
 async fn create_server() {
-    configure_server_basic(&server);
-    configure_request_middleware(&server);
-    configure_routes(&server);
-    configure_response_middleware(&server);
+    configure_server_basic(&server).await;
+    configure_request_middleware(&server).await;
+    configure_routes(&server).await;
+    configure_response_middleware(&server).await;
     println_success!("Server initialization successful");
-    match server.run() {
+    let server_result: ServerResult<()> = server.run().await;
+    match server_result {
         Ok(_) => {
             let host_port: String = format!("{SERVER_HOST}:{SERVER_PORT}");
             println_success!("Server listen in: ", host_port)
