07eed69461941c4dfea7b0ca4e8e13fffafe91b3
尤雨东 <root@ltpp.vip>
2025-07-20 13:56:32 +0800

    update: code

--- app/middleware/request/response/fn.rs
+++ app/middleware/request/response/fn.rs
@@ -6,11 +6,11 @@ use super::*;
 pub async fn response_header(ctx: Context) {
     let socket_addr_string: String = ctx.get_socket_addr_or_default_string().await;
     let content_type: String = ContentType::format_content_type_with_charset(TEXT_HTML, UTF8);
-    ctx.set_response_header(CONTENT_TYPE, content_type)
+    ctx.set_response_version(HttpVersion::HTTP1_1)
+        .await
+        .set_response_header(CONTENT_TYPE, content_type)
         .await
         .set_response_header("SocketAddr", socket_addr_string)
-        .await
-        .set_response_version(HttpVersion::HTTP1_1)
         .await;
 }
 
