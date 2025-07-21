efcabb2b91df09dbcf240f09d8e119ab774c7a66
尤雨东 <root@ltpp.vip>
2025-07-21 08:31:09 +0800

    update: code

--- app/middleware/request/response/fn.rs
+++ app/middleware/request/response/fn.rs
@@ -1,18 +1,21 @@
 use super::*;
 
+#[response_status_code(404)]
+pub async fn response_status_code(ctx: Context) {}
+
+#[response_header(DATE => gmt())]
 #[response_header(SERVER => HYPERLANE)]
 #[response_header(CONNECTION => KEEP_ALIVE)]
-#[response_header(DATE => gmt())]
+#[response_header(CONTENT_TYPE => TEXT_HTML)]
+#[response_version(HttpVersion::HTTP1_1)]
 pub async fn response_header(ctx: Context) {
     let socket_addr_string: String = ctx.get_socket_addr_or_default_string().await;
-    ctx.set_response_version(HttpVersion::HTTP1_1)
+    let content_type: String = ContentType::format_content_type_with_charset(TEXT_HTML, UTF8);
+    ctx.set_response_header(CONTENT_TYPE, content_type)
         .await
         .set_response_header("SocketAddr", socket_addr_string)
         .await;
 }
-
-#[response_status_code(404)]
-pub async fn response_status_code(ctx: Context) {}
 
 #[response_body(NOT_FOUND_HTML)]
 pub async fn response_body(ctx: Context) {}
