2ee4cbfe11d8704262d1043161e4666250396ed0
尤雨东 <root@ltpp.vip>
2025-07-22 23:11:32 +0800

    feat: v1.64.0

--- app/exception/framework/fn.rs
+++ app/exception/framework/fn.rs
@@ -10,11 +10,14 @@ pub async fn error_hook(ctx: Context) {
         response_body.push_str(&request_string);
         response_body.push_str(BR);
     }
-    println_error!(response_body);
+    println_error!("{}", response_body);
+    log_error(response_body.clone()).await;
     let _ = ctx
         .set_response_status_code(500)
         .await
         .clear_response_headers()
+        .await
+        .set_response_header(SERVER, HYPERLANE)
         .await
         .set_response_header(CONTENT_TYPE, content_type)
         .await
