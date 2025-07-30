51859d6dceea9c81e20089576ce3d6853fd4fb7b
尤雨东 <root@ltpp.vip>
2025-07-30 23:47:13 +0800

    feat: v1.66.0

--- app/middleware/response/send/fn.rs
+++ app/middleware/response/send/fn.rs
@@ -1,5 +1,10 @@
 use super::*;
 
 #[send]
+#[http]
 #[flush]
-pub async fn send(ctx: Context) {}
+pub async fn send(ctx: Context) {
+    if ctx.get_request_upgrade_type().await.is_ws() {
+        return;
+    }
+}
