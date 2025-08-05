a0d1fdbce2c2c2ac188330f81db1df93228022ff
尤雨东 <root@ltpp.vip>
2025-08-05 21:04:10 +0800

    feat: v1.66.16

--- app/controller/favicon_ico/fn.rs
+++ app/controller/favicon_ico/fn.rs
@@ -1,6 +1,14 @@
 use super::*;
 
 #[get]
+#[utoipa::path(
+    get,
+    path = "/favicon.ico",   
+    responses(
+        (status = 200, description = "Icon", body = String)
+    )
+)]
+#[get]
 #[response_status_code(301)]
 #[response_header(LOCATION => LOGO_IMG_URL)]
 pub async fn handle(ctx: Context) {}
--- app/controller/favicon_ico/mod.rs
+++ app/controller/favicon_ico/mod.rs
@@ -4,4 +4,3 @@ pub use r#fn::*;
 
 use super::*;
 use hyperlane_config::business::logo_img::*;
-use hyperlane_config::framework::CACHE_CONTROL_SHORT_TERM;
