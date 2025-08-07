05f896430a6a2b5f155be879a2fa6a3c87fe81e5
尤雨东 <root@ltpp.vip>
2025-08-07 22:42:08 +0800

    feat: v2.0.0

--- app/exception/framework/fn.rs
+++ app/exception/framework/fn.rs
@@ -4,6 +4,7 @@ pub async fn panic_hook(ctx: Context) {
     let error: Panic = ctx.get_panic().await.unwrap_or_default();
     let response_body: String = error.to_string();
     println_error!(response_body);
+    log_error(response_body.clone()).await;
     let content_type: String = ContentType::format_content_type_with_charset(TEXT_PLAIN, UTF8);
     let _ = ctx
         .set_response_version(HttpVersion::HTTP1_1)
