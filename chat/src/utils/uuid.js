5dbf95d13e92d4b1d4fc9b3327e3c80ee6b19134
尤雨东 <root@ltpp.vip>
2025-08-10 10:17:09 +0800

    feat: 2025-08-10 10:17:09

--- chat/src/utils/uuid.js
+++ chat/src/utils/uuid.js
@@ -1,8 +1,10 @@
+import { v4 as uuidv4 } from 'uuid';
+
 export const getPersistentUUID = function () {
   const KEY = 'browser_uuid';
   let uuid = localStorage.getItem(KEY);
   if (!uuid) {
-    uuid = crypto.randomUUID();
+    uuid = uuidv4();
     localStorage.setItem(KEY, uuid);
   }
   return uuid;
