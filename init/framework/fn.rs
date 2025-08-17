f239c6cf9db0f672264f3cd11d3f0e9129c62eea
尤雨东 <root@ltpp.vip>
2025-08-17 17:59:29 +0800

    feat: v3.5.0

--- init/framework/fn.rs
+++ init/framework/fn.rs
@@ -1,6 +1,6 @@
 use super::*;
 
-#[hyperlane(ServerConfig => config)]
+#[hyperlane(config: ServerConfig)]
 async fn configure_config(server: &Server) {
     config.host(SERVER_HOST).await;
     config.port(SERVER_PORT).await;
@@ -62,7 +62,7 @@ fn runtime() -> Runtime {
         .unwrap()
 }
 
-#[hyperlane(Server => server)]
+#[hyperlane(server: Server)]
 async fn create_server() {
     configure_config(&server).await;
     configure_panic_hook(&server).await;
