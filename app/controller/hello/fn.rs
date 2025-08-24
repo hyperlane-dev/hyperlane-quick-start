8393e21cb87ca33c2b865387f3f61b9ca29b6360
尤雨东 <root@ltpp.vip>
2025-08-24 21:49:54 +0800

    feat: v5.0.2

--- app/controller/hello/fn.rs
+++ app/controller/hello/fn.rs
@@ -1,10 +1,10 @@
 use super::*;
 
 #[route("/hello/{name}")]
-#[prologue_hooks[
+#[prologue_hooks(
   post,
   response_status_code(200),
   route_param(NAME_KEY => name_opt),
   response_body(format!("Hello {}", name_opt.unwrap_or_default())),
-]]
+)]
 pub async fn handle(ctx: Context) {}
--- app/controller/ws/fn.rs
+++ app/controller/ws/fn.rs
@@ -1,10 +1,7 @@
 use super::*;
 
 #[route("/websocket")]
-#[prologue_hooks[
-    ws,
-    get
-]]
+#[prologue_hooks(ws, get)]
 pub async fn handle(ctx: Context) {
     let request_body: Vec<u8> = ctx.get_request_body().await;
     let _ = ctx.set_response_body(request_body).await.send_body().await;
