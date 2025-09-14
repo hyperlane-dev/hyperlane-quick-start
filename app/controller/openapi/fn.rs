c2671d73538aae2a62502f0b043534d8c491c728
尤雨东 <root@ltpp.vip>
2025-09-14 17:48:59 +0800

    feat: v6.2.1

--- app/controller/openapi/fn.rs
+++ app/controller/openapi/fn.rs
@@ -11,7 +11,7 @@ use super::*;
 #[prologue_hooks(methods(get, post), response_status_code(200))]
 pub async fn json(ctx: Context) {
     if let Ok(json_data) = ApiDoc::openapi().to_json() {
-        let _ = ctx.set_response_body(&json_data).await.send().await;
+        let _ = ctx.set_response_body(&json_data).await;
     }
 }
 
@@ -31,5 +31,5 @@ pub async fn json(ctx: Context) {
 pub async fn html(ctx: Context) {
     SwaggerUi::new("/openapi/{file}").url("/openapi/openapi.json", ApiDoc::openapi());
     let res: String = RapiDoc::with_openapi("/openapi/openapi.json", ApiDoc::openapi()).to_html();
-    let _ = ctx.set_response_body(&res).await.send().await;
+    let _ = ctx.set_response_body(&res).await;
 }
