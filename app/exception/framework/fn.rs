cb14a6a2e5ad5bac558a8c6d283385c8b414df91
尤雨东 <root@ltpp.vip>
2025-09-14 17:49:42 +0800

    feat: v6.2.2

--- app/exception/framework/fn.rs
+++ app/exception/framework/fn.rs
@@ -1,25 +1,19 @@
 use super::*;
 
-#[send]
 #[panic_hook]
-#[prologue_hooks(
+#[epilogue_hooks(
     response_status_code(500),
+    response_body(&response_body),
     response_header(SERVER => HYPERLANE),
-    response_version(HttpVersion::HTTP1_1)
+    response_version(HttpVersion::HTTP1_1),
+    response_header(CONTENT_TYPE, &content_type),
+    send
 )]
 pub async fn panic_hook(ctx: Context) {
     let error: Panic = ctx.try_get_panic().await.unwrap_or_default();
     let response_body: String = error.to_string();
-    let content_type: String = ContentType::format_content_type_with_charset(TEXT_PLAIN, UTF8);
-    let _ = ctx
-        .clear_response_headers()
-        .await
-        .set_response_header(CONTENT_TYPE, &content_type)
-        .await
-        .set_response_body(&response_body)
-        .await
-        .send()
-        .await;
     log_error(&response_body).await;
     println_error!(response_body);
+    let content_type: String = ContentType::format_content_type_with_charset(TEXT_PLAIN, UTF8);
+    let _ = ctx.clear_response_headers().await;
 }
