1290f492cd7fdb1010952e7a3a4a30d8a87d968e
尤雨东 <root@ltpp.vip>
2025-09-27 12:50:06 +0800

    feat: v6.3.6

--- config/business/not_found/const.rs
+++ config/business/not_found/const.rs
@@ -1,1 +1,2 @@
-pub const NOT_FOUND_HTML: &'static str = include_str!("../../../resources/static/html/404.html");
+pub const NOT_FOUND_HTML: &'static str =
+    include_str!("../../../resources/static/not_found/index.html");
