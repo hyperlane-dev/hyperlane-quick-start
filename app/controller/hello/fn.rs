da79d07f971826e79738f914e03ad756e71d46d1
尤雨东 <root@ltpp.vip>
2025-09-27 18:24:46 +0800

    feat: v7.0.0

--- app/controller/hello/fn.rs
+++ app/controller/hello/fn.rs
@@ -1,7 +1,7 @@
 use super::*;
 
 #[route("/hello/{name}")]
-#[prologue_hooks(
+#[prologue_macros(
   methods(get, post),
   route_param(NAME_KEY => name_opt),
   response_body(format!("Hello {}", name_opt.unwrap_or_default())),
