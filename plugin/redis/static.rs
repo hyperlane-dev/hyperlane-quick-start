378b3c25b2894f86e55be2dbe7557472b1ba99c8
尤雨东 <root@ltpp.vip>
2025-10-12 13:07:09 +0800

    feat: v7.5.0

--- plugin/redis/static.rs
+++ plugin/redis/static.rs
@@ -1,4 +1,4 @@
 use super::*;
 
-pub static REDIS_DB: Lazy<Arc<Connection>> =
+pub static REDIS_DB: Lazy<Result<Arc<Connection>, String>> =
     Lazy::new(|| block_on(async { connection_redis_db().await }));
