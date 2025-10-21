3703e4fbd1829cf0c2816fe5a3e9e50609e9c937
尤雨东 <root@ltpp.vip>
2025-10-21 22:22:53 +0800

    feat: v8.0.0

--- app/mapper/openapi/mod.rs
+++ app/mapper/openapi/mod.rs
@@ -1,3 +1,4 @@
+mod r#impl;
 mod r#struct;
 
 pub use r#struct::*;
@@ -7,4 +8,17 @@ use model::{
     application::{chat::*, monitor::*, upload::*},
     data_transfer::{chat::*, upload::*},
     param::chat::*,
+};
+
+use std::collections::BTreeMap;
+
+use utoipa::{
+    Modify,
+    openapi::{
+        ContentBuilder, HttpMethod, ObjectBuilder, OpenApi, PathItem, Ref, Required,
+        ResponseBuilder, ResponsesBuilder, Type,
+        path::{OperationBuilder, ParameterBuilder, ParameterIn},
+        request_body::RequestBodyBuilder,
+        schema::SchemaType,
+    },
 };
