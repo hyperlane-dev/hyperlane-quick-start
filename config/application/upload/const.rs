059a967cd138f6769b1ca578bb62e9d1a06984bd
尤雨东 <root@ltpp.vip>
2025-10-21 23:47:31 +0800

    feat: v8.0.0

--- config/application/upload/const.rs
+++ config/application/upload/const.rs
@@ -1,6 +1,6 @@
 pub const UPLOAD_DIR: &str = "/shell/upload";
 pub const CHUNKIFY_DIRECTORY_HEADER: &str = "x-directory-path";
-pub const STATIC_ROUTE: &str = "static";
+pub const STATIC_ROUTE: &str = "upload/file";
 pub const UPLOAD_DIR_KEY: &str = "upload_dir";
 pub const UPLOAD_FILE_KEY: &str = "upload_file";
 pub const PARTIAL_CONTENT_STATUS: usize = 206;
