af9de1a10df7dc070ea3ac790c66b9051ee31a4e
尤雨东 <root@ltpp.vip>
2025-11-12 22:53:28 +0800

    feat: v8.6.2

--- app/service/rss/impl.rs
+++ app/service/rss/impl.rs
@@ -47,8 +47,29 @@ impl RssService {
         } else {
             String::new()
         };
-        let url_encode_path: String = Encode::execute(CHARSETS, &relative_path).unwrap_or_default();
-        let file_url: String = format!("/{STATIC_ROUTE}/{url_encode_path}");
+        let file_url: String = if let Some(parent_path) = path.parent() {
+            let parent_str: String = parent_path.to_string_lossy().to_string();
+            let dir_path: String = parent_str
+                .replace(UPLOAD_DIR, EMPTY_STR)
+                .replace('\\', ROOT_PATH)
+                .trim_start_matches(ROOT_PATH)
+                .to_string();
+            if dir_path.is_empty() {
+                let url_encode_file_name: String =
+                    Encode::execute(CHARSETS, &file_name).unwrap_or_default();
+                format!("/{STATIC_ROUTE}/{url_encode_file_name}")
+            } else {
+                let url_encode_dir: String =
+                    Encode::execute(CHARSETS, &dir_path).unwrap_or_default();
+                let url_encode_file_name: String =
+                    Encode::execute(CHARSETS, &file_name).unwrap_or_default();
+                format!("/{STATIC_ROUTE}/{url_encode_dir}/{url_encode_file_name}")
+            }
+        } else {
+            let url_encode_file_name: String =
+                Encode::execute(CHARSETS, &file_name).unwrap_or_default();
+            format!("/{STATIC_ROUTE}/{url_encode_file_name}")
+        };
         let extension_name: String = FileExtension::get_extension_name(&file_name);
         let content_type: String = FileExtension::parse(&extension_name)
             .get_content_type()
