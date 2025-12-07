8003ed1b845641c13a93853cdae551155da14558
尤雨东 <root@ltpp.vip>
2025-12-07 11:47:30 +0800

    feat: v8.8.0

--- config/framework/const.rs
+++ config/framework/const.rs
@@ -8,7 +8,6 @@ pub const SERVER_HOST: &str = DEFAULT_HOST;
 pub const SERVER_BUFFER: usize = DEFAULT_BUFFER_SIZE;
 pub const SERVER_LOG_SIZE: usize = 100_024_000;
 pub const SERVER_LOG_DIR: &str = "/shell/logs";
-pub const SERVER_LOG_LEVEL: [&str; 3] = ["info", "warn", "error"];
 pub const SERVER_INNER_PRINT: bool = true;
 pub const SERVER_INNER_LOG: bool = true;
 pub const SERVER_NODELAY: bool = false;
@@ -16,6 +15,7 @@ pub const SERVER_LINGER: OptionDuration = None;
 pub const SERVER_TTI: u32 = 128;
 pub const SERVER_PID_FILE_PATH: &str = "./tmp/process/hyperlane.pid";
 
+pub const SERVER_LOG_LEVEL: [&str; 3] = ["info", "warn", "error"];
 pub const CACHE_CONTROL_STATIC_ASSETS: &str = "public, max-age=31536000, immutable";
 pub const CACHE_CONTROL_SHORT_TERM: &str = "public, max-age=3600";
 pub const EXPIRES_FAR_FUTURE: &str = "Wed, 1 Apr 8888 00:00:00 GMT";
