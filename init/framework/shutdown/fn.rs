9a79a5ec519b28c73f2ea564686c4d2d239de78d
尤雨东 <root@ltpp.vip>
2026-01-11 11:39:44 +0800

    feat: v8.16.9

--- init/framework/shutdown/fn.rs
+++ init/framework/shutdown/fn.rs
@@ -1,19 +1,19 @@
 use super::*;
 
+fn default_shutdown() -> SharedAsyncTaskFactory<()> {
+    Arc::new(|| {
+        Box::pin(async {
+            warn!("Not set shutdown, using default");
+        })
+    })
+}
+
 pub fn set_shutdown(shutdown: SharedAsyncTaskFactory<()>) {
     trace!("Set shutdown");
-    let _ = SHUTDOWN.set(shutdown);
+    drop(SHUTDOWN.set(shutdown));
 }
 
 pub fn get_shutdown() -> SharedAsyncTaskFactory<()> {
     trace!("Get shutdown");
-    SHUTDOWN
-        .get_or_init(|| {
-            Arc::new(|| {
-                Box::pin(async {
-                    warn!("Not set shutdown, using default");
-                })
-            })
-        })
-        .clone()
+    SHUTDOWN.get_or_init(default_shutdown).clone()
 }
