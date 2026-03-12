45de811245404bb10d559c8dd7fb3e2e56f10421
尤雨东 <root@ltpp.vip>
2026-03-12 23:31:59 +0800

    feat: v18.1.2

--- resources/static/common/js/common.js
+++ resources/static/common/js/common.js
@@ -1,15 +1,15 @@
 const HyperlaneCommon = {
-  ...Toast,
-  ...DateUtil,
-  ...NumberUtil,
-  ...FunctionUtil,
-  ...DOMUtil,
-  ...URLUtil,
-  ...StringUtil,
-  ...ValidateUtil,
-  ...StorageUtil,
-  ...ObjectUtil,
-  ...FileUtil,
+  ...(typeof Toast !== 'undefined' ? Toast : {}),
+  ...(typeof DateUtil !== 'undefined' ? DateUtil : {}),
+  ...(typeof NumberUtil !== 'undefined' ? NumberUtil : {}),
+  ...(typeof FunctionUtil !== 'undefined' ? FunctionUtil : {}),
+  ...(typeof DOMUtil !== 'undefined' ? DOMUtil : {}),
+  ...(typeof URLUtil !== 'undefined' ? URLUtil : {}),
+  ...(typeof StringUtil !== 'undefined' ? StringUtil : {}),
+  ...(typeof ValidateUtil !== 'undefined' ? ValidateUtil : {}),
+  ...(typeof StorageUtil !== 'undefined' ? StorageUtil : {}),
+  ...(typeof ObjectUtil !== 'undefined' ? ObjectUtil : {}),
+  ...(typeof FileUtil !== 'undefined' ? FileUtil : {}),
 };
 
 if (typeof module !== 'undefined' && module.exports) {
