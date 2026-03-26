2d19da40f79dae0d7474be9be18086c14b07f992
尤雨东 <root@ltpp.vip>
2026-03-26 23:54:22 +0800

    feat: v18.5.0

--- resources/sql/postgresql/order/data.sql
+++ resources/sql/postgresql/order/data.sql
@@ -7,7 +7,7 @@ INSERT INTO
     )
 VALUES (
         'root',
-        '4813494d137e1631bba301d5acab6e7bb7aa74ce1185d456565ef51d737677b2',
+        '63a9f0ea7bb98050796b649e85481845',
         1,
         1
     )
