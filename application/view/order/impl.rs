0db3413cfeeb35655893f3ac66ebca70dbc6ee4e
尤雨东 <root@ltpp.vip>
2026-04-16 19:38:38 +0800

    feat: v21.0.10

--- application/view/order/impl.rs
+++ application/view/order/impl.rs
@@ -6,17 +6,16 @@ impl ServerHook for OrderViewRoute {
         Self
     }
 
-    #[prologue_macros(methods(get, post))]
+    #[prologue_macros(
+        methods(get, post),
+        response_status_code(302),
+        response_header(LOCATION => "/static/order/index.html")
+    )]
     #[instrument_trace]
     async fn handle(self, ctx: &mut Context) {
         let is_authenticated: bool = ctx.get_request().try_get_cookie(TOKEN).is_some();
-        if is_authenticated {
+        if !is_authenticated {
             ctx.get_mut_response()
-                .set_status_code(302)
-                .set_header(LOCATION, "/order");
-        } else {
-            ctx.get_mut_response()
-                .set_status_code(302)
                 .set_header(LOCATION, format!("/auth?{LOCATION}=/order"));
         }
     }
