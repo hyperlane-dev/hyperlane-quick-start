8e1d8ed8e014e9308f49831b579694495b0954ba
尤雨东 <root@ltpp.vip>
2026-05-05 22:45:46 +0800

    feat: v21.3.0

--- application/mapper/tracking/impl.rs
+++ application/mapper/tracking/impl.rs
@@ -1,3 +1,9 @@
 use super::*;
 
 impl ActiveModelBehavior for ActiveModel {}
+
+impl RelationTrait for Relation {
+    fn def(&self) -> RelationDef {
+        panic!("No relations defined")
+    }
+}
