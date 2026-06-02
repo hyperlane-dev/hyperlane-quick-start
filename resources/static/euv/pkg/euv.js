8666196169cb343dcac0ed132f81a858217b912a
sheng <root@ltpp.vip>
2026-06-02 23:45:19 +0800

    feat: v23.0.9

--- resources/static/euv/pkg/euv.js
+++ resources/static/euv/pkg/euv.js
@@ -816,7 +816,7 @@ function __wbg_get_imports() {
         },
         __wbindgen_cast_0000000000000001: function(arg0, arg1) {
             // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [Externref], shim_idx: 1, ret: Result(Unit), inner_ret: Some(Result(Unit)) }, mutable: true }) -> Externref`.
-            const ret = makeMutClosure(arg0, arg1, __wasm_bindgen_func_elem_1305);
+            const ret = makeMutClosure(arg0, arg1, __wasm_bindgen_func_elem_1302);
             return addHeapObject(ret);
         },
         __wbindgen_cast_0000000000000002: function(arg0, arg1) {
@@ -831,7 +831,7 @@ function __wbg_get_imports() {
         },
         __wbindgen_cast_0000000000000004: function(arg0, arg1) {
             // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [], shim_idx: 8, ret: Unit, inner_ret: Some(Unit) }, mutable: true }) -> Externref`.
-            const ret = makeMutClosure(arg0, arg1, __wasm_bindgen_func_elem_1220);
+            const ret = makeMutClosure(arg0, arg1, __wasm_bindgen_func_elem_1218);
             return addHeapObject(ret);
         },
         __wbindgen_cast_0000000000000005: function(arg0, arg1) {
@@ -853,8 +853,8 @@ function __wbg_get_imports() {
     };
 }
 
-function __wasm_bindgen_func_elem_1220(arg0, arg1) {
-    wasm.__wasm_bindgen_func_elem_1220(arg0, arg1);
+function __wasm_bindgen_func_elem_1218(arg0, arg1) {
+    wasm.__wasm_bindgen_func_elem_1218(arg0, arg1);
 }
 
 function __wasm_bindgen_func_elem_690(arg0, arg1, arg2) {
@@ -865,10 +865,10 @@ function __wasm_bindgen_func_elem_690_2(arg0, arg1, arg2) {
     wasm.__wasm_bindgen_func_elem_690_2(arg0, arg1, addHeapObject(arg2));
 }
 
-function __wasm_bindgen_func_elem_1305(arg0, arg1, arg2) {
+function __wasm_bindgen_func_elem_1302(arg0, arg1, arg2) {
     try {
         const retptr = wasm.__wbindgen_add_to_stack_pointer(-16);
-        wasm.__wasm_bindgen_func_elem_1305(retptr, arg0, arg1, addHeapObject(arg2));
+        wasm.__wasm_bindgen_func_elem_1302(retptr, arg0, arg1, addHeapObject(arg2));
         var r0 = getDataViewMemory0().getInt32(retptr + 4 * 0, true);
         var r1 = getDataViewMemory0().getInt32(retptr + 4 * 1, true);
         if (r1) {
