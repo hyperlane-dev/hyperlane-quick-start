9f58fbd1a347f00ab615c4fe425cdd16d46fb798
sheng <root@ltpp.vip>
2026-07-11 20:27:21 +0800

    feat: v23.0.24

--- bootstrap/framework/config/impl.rs
+++ bootstrap/framework/config/impl.rs
@@ -14,12 +14,13 @@ impl BootstrapAsyncInit for ConfigBootstrap {
         request_config
             .set_max_body_size(env_config.get_server_request_max_body_size())
             .set_read_timeout_ms(env_config.get_server_request_http_read_timeout_ms());
-        server_config.set_address(Server::format_bind_address(
-            env_config.get_server_host(),
-            env_config.get_server_port(),
-        ));
-        server_config.set_ttl(env_config.get_server_tti());
-        server_config.set_nodelay(env_config.get_server_nodelay());
+        server_config
+            .set_address(Server::format_bind_address(
+                env_config.get_server_host(),
+                env_config.get_server_port(),
+            ))
+            .set_ttl(env_config.get_server_tti())
+            .set_nodelay(env_config.get_server_nodelay());
         debug!("Server config {server_config:?}");
         info!("Server initialization successful");
         Self {
